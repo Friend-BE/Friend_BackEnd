b8c689c341981ad319731ad592f7110af3ec9869
최민석 <cms8955@naver.com>
2024-01-11 18:40:42 +0900

    feat: UnivCert

---
+++ src/main/java/com/friend/friend/controller/UnivCertController.java
@@ -0,0 +1,74 @@
+package com.friend.friend.controller;
+
+import com.friend.friend.domain.UnivCertResponse;
+import com.friend.friend.domain.StatusEnum;
+import com.friend.friend.dto.MailDTO;
+import com.univcert.api.UnivCert;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+
+@Configuration
+@RestController
+@RequestMapping("/api/v1")
+public class UnivCertController {
+    @Value("${univCert.key}")
+    private String key;
+
+    @PostMapping("/certify/send")
+    public ResponseEntity<UnivCertResponse> sendUnivCertMail(@RequestBody MailDTO mailDTO) throws IOException {
+        UnivCert.clear(key, mailDTO.getEmail());
+        Map<String, Object> check = UnivCert.check(mailDTO.getUnivName());
+        boolean univ_check = (boolean) check.get("success");
+
+        Map<String, Object> result = UnivCert.certify(key, mailDTO.getEmail(), mailDTO.getUnivName(), univ_check);
+        boolean emailSuccess = (boolean) result.get("success");
+
+        UnivCertResponse message = new UnivCertResponse();
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(new MediaType("application", "json", StandardCharsets.UTF_8));
+
+        if (emailSuccess) {
+            message.setStatus(StatusEnum.OK);
+            message.setMessage("Success at sending mail");
+        } else {
+            message.setStatus(StatusEnum.BAD_REQUEST);
+            message.setMessage("Error at sending mail");
+        }
+
+        return new ResponseEntity<>(message, httpHeaders, emailSuccess ? HttpStatus.OK : HttpStatus.BAD_REQUEST);
+    }
+
+
+    @GetMapping("/certify/verify")
+    public ResponseEntity<UnivCertResponse> validMailCode(@RequestParam String email,
+                                                          @RequestParam String univName,
+                                                          @RequestParam int code) throws IOException {
+
+        Map<String, Object> response = UnivCert.certifyCode(key, email, univName, code);
+        boolean success = (boolean) response.get("success");
+
+        UnivCertResponse message = new UnivCertResponse();
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(new MediaType("application", "json", StandardCharsets.UTF_8));
+
+        if (success) {
+            message.setStatus(StatusEnum.OK);
+            message.setMessage("Email verification succeeded.");
+            return new ResponseEntity<>(message, httpHeaders, HttpStatus.OK);
+        } else {
+            message.setStatus(StatusEnum.BAD_REQUEST);
+            message.setMessage("Email verification failed.");
+            return new ResponseEntity<>(message, httpHeaders, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+}
