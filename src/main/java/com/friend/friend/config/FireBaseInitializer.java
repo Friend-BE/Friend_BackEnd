054504857af77460e0764a05bfbe1d2b44767bb1
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2024-01-17 00:41:49 +0900

    adasdas

--- src/main/java/com/friend/friend/config/FireBaseInitializer.java
+++ src/main/java/com/friend/friend/config/FireBaseInitializer.java
@@ -14,22 +14,22 @@ import org.springframework.stereotype.Service;
 @Service
 public class FireBaseInitializer {
 
-    @Value("${app.firebase-configuration-file}")
-    private String firebaseConfigPath;
-
-    @PostConstruct
-    public void initialize() {
-        try {
-            FirebaseOptions options = new FirebaseOptions.Builder().setCredentials(
-                    GoogleCredentials.fromStream(
-                            new ClassPathResource(firebaseConfigPath).getInputStream())).build();
-            if (FirebaseApp.getApps().isEmpty()) {
-                FirebaseApp.initializeApp(options);
-                log.info("Firebase application has been initialized");
-            }
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        }
-
-    }
+//    @Value("${app.firebase-configuration-file}")
+//    private String firebaseConfigPath;
+//
+//    @PostConstruct
+//    public void initialize() {
+//        try {
+//            FirebaseOptions options = new FirebaseOptions.Builder().setCredentials(
+//                    GoogleCredentials.fromStream(
+//                            new ClassPathResource(firebaseConfigPath).getInputStream())).build();
+//            if (FirebaseApp.getApps().isEmpty()) {
+//                FirebaseApp.initializeApp(options);
+//                log.info("Firebase application has been initialized");
+//            }
+//        } catch (IOException e) {
+//            log.error(e.getMessage());
+//        }
+//
+//    }
 }
