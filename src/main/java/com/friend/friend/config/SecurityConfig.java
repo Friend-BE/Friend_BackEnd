78e9bff1b2fffc01a428e3d5cad083ded5bdf4a5
최민석 <cms8955@naver.com>
2024-01-21 01:29:01 +0900

    feat: CORS 허용

--- src/main/java/com/friend/friend/config/SecurityConfig.java
+++ src/main/java/com/friend/friend/config/SecurityConfig.java
@@ -9,10 +9,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
 
-import java.util.Collections;
+import static org.springframework.security.config.Customizer.withDefaults;
 
 
 @Configuration
@@ -21,18 +19,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable);
-        http.cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
-            @Override
-            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-                CorsConfiguration config = new CorsConfiguration();
-                config.setAllowedOrigins(Collections.singletonList("http://13.209.145.28"));
-                config.setAllowedMethods(Collections.singletonList("*"));
-                config.setAllowCredentials(true);
-                config.setAllowedHeaders(Collections.singletonList("*"));
-                config.setMaxAge(3600L); //1시간
-                return config;
-            }
-        }));
+        http.cors(withDefaults());
         http.authorizeHttpRequests(
                         authorize -> authorize
                                 .requestMatchers( "/","/swagger-ui/**", "/v3/api-docs/**","/hello","/login","/api/v1/**").permitAll()
