89bbe77461e097588a6fda23b02486f8f89a3b90
Jung_Hun_Chae <50732828+chaesc1@users.noreply.github.com>
2024-01-23 18:57:02 +0900

    :fire: feat : 공통응답API 제작

--- src/main/java/com/friend/friend/service/NoticeService.java
+++ src/main/java/com/friend/friend/service/NoticeService.java
@@ -1,15 +1,17 @@
 package com.friend.friend.service;
 
-import com.friend.friend.domain.board.Board;
+import com.friend.friend.common.Response;
 import com.friend.friend.domain.board.Notice;
-import com.friend.friend.dto.BoardResponseDto;
 import com.friend.friend.dto.NoticeRequestDto;
 import com.friend.friend.dto.NoticeResponseDto;
 import com.friend.friend.dto.SuccessResponseDto;
 import com.friend.friend.repository.NoticeRepository;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
-import org.aspectj.weaver.ast.Not;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,24 +26,27 @@ public class NoticeService {
     }
 
     @Transactional
-    public NoticeResponseDto createNotice(NoticeRequestDto requestDto) {
+    public boolean createNotice(NoticeRequestDto requestDto) {
         Notice notice = new Notice(requestDto);
-        System.out.println(notice);
-        noticeRepository.save(notice);
-        return new NoticeResponseDto(notice);
+//        noticeRepository.save(notice);
+//        return new NoticeResponseDto(notice);
+        if (notice == null) {
+            return false;
+        } else {
+            noticeRepository.save(notice);
+            return true;
+        }
     }
 
     @Transactional
-    public NoticeResponseDto getPost(Long id) {
-        return noticeRepository.findById(id).map(NoticeResponseDto::new).orElseThrow(
-                () -> new IllegalArgumentException("존재하지 않는 아이디 입니다")
-        );
+    public Optional<NoticeResponseDto> getPost(Long id) {
+        return noticeRepository.findById(id).map(NoticeResponseDto::new);
     }
 
     @Transactional
-    public NoticeResponseDto updateNotice(Long id, NoticeRequestDto requestDto) throws Exception {
+    public NoticeResponseDto updateNotice(Long id, NoticeRequestDto requestDto) throws Exception{
         Notice notice = noticeRepository.findById(id).orElseThrow(
-                () -> new IllegalArgumentException("존재하지 않는 아이디 입니다.")
+                () -> new IllegalArgumentException("존재하지 않는 게시물 입니다")
         );
 
         notice.update(requestDto);
@@ -49,7 +54,7 @@ public class NoticeService {
     }
 
     @Transactional
-    public SuccessResponseDto deletePost(Long id, NoticeRequestDto requestDto) throws Exception{
+    public SuccessResponseDto deletePost(Long id, NoticeRequestDto requestDto) throws Exception {
         Notice notice = noticeRepository.findById(id).orElseThrow(
                 () -> new IllegalArgumentException("존재하지 않는 게시물 입니다")
         );
