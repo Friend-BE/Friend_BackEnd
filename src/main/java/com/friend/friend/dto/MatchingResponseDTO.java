f388aaf75d412b8f306dd042e23cee6dfe6dc0e0
최민석 <cms8955@naver.com>
2024-02-03 17:33:31 +0900

    feat: 신고하기 가능한 매칭당사자 출력

--- src/main/java/com/friend/friend/dto/MatchingResponseDTO.java
+++ src/main/java/com/friend/friend/dto/MatchingResponseDTO.java
@@ -26,16 +26,16 @@ public class MatchingResponseDTO {
         private MatchingStatusEnum status; //매칭완료여부
         private String bithday; //상대방 생년월일
     }
-//    @Builder
-//    @Getter
-//    @NoArgsConstructor
-//    @AllArgsConstructor
-//    public static class getReportListDTO {
-//        private Long id;
-//        @JsonFormat(shape = JsonFormat.Shape.STRING,pattern="yyyy-MM-dd")
-//        private LocalDateTime date;   //날짜
-//        private String opponent;    //상대방
-////        private AnswerStatusEnum status; //매칭완료여부
-////        private String bithday; //상대방 생년월일
-//    }
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class getReportListDTO {
+        private Long id;
+        @JsonFormat(shape = JsonFormat.Shape.STRING,pattern="yyyy-MM-dd")
+        private LocalDateTime date;   //날짜
+        private String opponentNickname;    //상대방
+        private String opponentBirthday; //상대방 생년월일
+        private String opponentDepartment;  //상대방 대학
+    }
 }
