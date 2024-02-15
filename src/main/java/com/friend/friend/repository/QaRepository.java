1abce2c289c261218dd6df17547eff52ad96e264
unknown <leejunho2000@gmail.com>
2024-02-15 22:10:25 +0900

    #229 나의문의하기 API 추가

--- src/main/java/com/friend/friend/repository/QaRepository.java
+++ src/main/java/com/friend/friend/repository/QaRepository.java
@@ -4,7 +4,10 @@ import com.friend.friend.domain.board.Qa;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface QaRepository extends JpaRepository<Qa,Long> {
 
+    List<Qa> findByAuthor(String Author);
 }
